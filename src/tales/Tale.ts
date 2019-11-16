c4d1c69996972a46ca479ca03de5dd9cba61068e
Prateek057 <bagrecha.prateek@gmail.com>
2019-11-16 18:46:22 +0100

    Tales Create

--- src/tales/Tale.ts
+++ src/tales/Tale.ts
@@ -1,9 +1,5 @@
 export interface Tale {
     author: String,
-    name: String,
-    part: Number,
-    totalParts: Number,
-    tags: [String],
-    animal: String,
-    animalName: String
+    description: String,
+    summary: String
 }
