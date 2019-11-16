cc6296c92ac8c9a2a6054b5e15a2010cee1547db
Akash-M <akash.manjunath@gmail.com>
2019-11-16 21:07:11 +0100

    remove code

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:13.0.1-alpine
-
-WORKDIR /usr/app
-
-COPY package.json /usr/app/
-RUN  npm install 
-
-COPY . .
--- frontend/tests/unit/dashboard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import Dashboard from '@/views/Dashboard.vue';
-
-describe('Dashboard.vue', () => {
-  test('renders dashboard component', () => {
-    const wrapper = shallowMount(Dashboard);
-    expect(wrapper.element).toMatchSnapshot();
-  });
-});
